fc65fcaf49a5f1f4f249dc34d12ef7a95b328543
Thakur Ballary <thakurballary@gmail.com>
2018-06-21 09:05:58 +0530

    Updated RadioButtons

--- src/Components/RadioButton.js
+++ src/Components/RadioButton.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { RadioButton } from 'react-native-btr';
 
 class RadioButtonDemo extends Component {
     
@@ -10,7 +11,34 @@ class RadioButtonDemo extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>RadioButton</Text>
+                <Text style={{fontWeight: 'bold', margin: 20}}>
+                    For functionality check 
+                    <Text style={{fontSize: 20}}>"RadioGroup"</Text>
+                </Text>
+                <View style={styles.card}>
+                    <View style={styles.row}><RadioButton /><Text style={styles.label}>Default</Text></View>
+                    <View style={styles.row}><RadioButton checked={true} /><Text style={styles.label}>checked</Text></View>
+                </View>
+                <View style={styles.card}>
+                    <View style={styles.row}>
+                        <RadioButton 
+                            checked={true}
+                            color='#484'
+                            size={14}
+                        />
+                        <Text style={styles.label}>color & size</Text>
+                    </View>
+                    <View style={styles.row}>
+                        <RadioButton 
+                            checked={true}
+                            color='#484'
+                            disabled={true}
+                            flexDirection='column'
+                            size={14}
+                        />
+                        <Text style={styles.label}>disabled</Text>
+                    </View>
+                </View>
             </View>
         );
     }
@@ -20,8 +48,22 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
+        margin: 20
     },
+    card : {
+        backgroundColor: '#fff', 
+        margin: 10, 
+        padding: 10, 
+        borderRadius: 4
+    },
+    row: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        margin: 5
+    },
+    label: {
+        marginHorizontal: 20
+    }
 });
 
 export default RadioButtonDemo;
--- src/Components/RadioGroup.js
+++ src/Components/RadioGroup.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { RadioGroup } from 'react-native-btr';
 
 class RadioGroupDemo extends Component {
 
@@ -7,10 +8,34 @@ class RadioGroupDemo extends Component {
         title: 'RadioGroup'
     }
 
+    state = {
+        radioButtons: [
+            {
+                label: 'Default',
+            },
+            {
+                label: 'In-Progress',
+                checked: true,
+                color: '#cb8'
+            },
+            {
+                label: 'Completed',
+                color: '#484'
+            },
+        ]
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>RadioGroup</Text>
+                <View style={styles.card}>
+                    <RadioGroup 
+                        labelStyle={{fontSize: 14}}
+                        radioButtons={this.state.radioButtons}
+                        onPress={radioButtons => this.setState({radioButtons})}
+                        style={{flexDirection: 'row'}}
+                    />
+                </View>
             </View>
         );
     }
@@ -19,9 +44,13 @@ class RadioGroupDemo extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
     },
+    card: {
+        backgroundColor: '#fff',
+        borderRadius: 4,
+        margin: 20, 
+        padding: 20
+    }
 });
 
 export default RadioGroupDemo;
