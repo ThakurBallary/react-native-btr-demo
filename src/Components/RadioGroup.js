fbdc9be91a865be1c9fe0a291fd5e875d9bfa194
ThakurBallary <thakurballary@gmail.com>
2018-06-21 11:18:08 +0530

    Updated RadioButton and RadioGroup

--- src/Components/RadioGroup.js
+++ src/Components/RadioGroup.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { RadioGroup } from 'react-native-btr';
 
 class RadioGroupDemo extends Component {
 
@@ -7,10 +8,51 @@ class RadioGroupDemo extends Component {
         title: 'RadioGroup'
     }
 
+    state = {
+        radioButtonsVertical: [
+            {
+                label: 'Default',
+            },
+            {
+                label: 'In-Progress',
+                checked: true,
+                color: '#f84'
+            },
+            {
+                label: 'Completed',
+                color: '#484'
+            },
+        ],
+        radioButtonsHorizontal: [
+            {
+                label: 'In-Progress',
+                flexDirection: 'column'
+            },
+            {
+                label: 'Completed',
+                flexDirection: 'column'
+            },
+        ]
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>RadioGroup</Text>
+                <View style={styles.card}>
+                    <RadioGroup
+                        labelStyle={{ fontSize: 14 }}
+                        radioButtons={this.state.radioButtonsVertical}
+                        onPress={radioButtonsVertical => this.setState({ radioButtonsVertical })}                        
+                    />
+                </View>
+                <View style={styles.card}>
+                    <RadioGroup
+                        labelStyle={{ fontSize: 14 }}
+                        radioButtons={this.state.radioButtonsHorizontal}
+                        onPress={radioButtonsHorizontal => this.setState({ radioButtonsHorizontal })}
+                        style={styles.radioButtonsHorizontal}
+                    />
+                </View>
             </View>
         );
     }
@@ -19,9 +61,17 @@ class RadioGroupDemo extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
     },
+    card: {
+        backgroundColor: '#fff',
+        borderRadius: 4,
+        margin: 20,
+        padding: 20
+    },
+    radioButtonsHorizontal: {
+        flexDirection: 'row', 
+        alignSelf: 'center'
+    }
 });
 
 export default RadioGroupDemo;
